1dd4b907ff4d9581c18b4e38e8719e83ba0dace1
jiahaowork <jiahaowork@gmail.com>
2018-04-22 10:43:59 +0800

    update comments

--- el-fly-indent-mode.el
+++ el-fly-indent-mode.el
@@ -24,7 +24,9 @@
 ;;; Commentary:
 
 ;; This minor mode indents Emacs Lisp code on the fly.
-;; After installation, no additional setup needed.
+;; After installation, add
+;; (add-hook 'emacs-lisp-mode-hook #'el-fly-indent-mode)
+;; to your ~/.emacs file.
 
 ;; For detailed explanations and more examples see the homepage:
 ;; https://github.com/jiahaowork/el-fly-indent-mode.el
